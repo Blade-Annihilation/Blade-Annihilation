94ded116e10483015282ce424d1f5a1edabf25c7
unknown <S10062870@W7SDTL1PJ1.ad.austinisd.org>
2015-02-09 13:54:17 -0600

    this is my audio runner so far

--- src/com/bladeannihilation/main/Audio.java
+++ src/com/bladeannihilation/main/Audio.java
@@ -1,6 +1,24 @@
 package com.bladeannihilation.main;	
 
-public class Audio {
-	static String turnDown = "turnDownForWhat";
-	
+import javax.sound.sampled.*;
+import java.io.*;
+
+public class Audio{
+	public void setSong(String songName){
+      String song = spongName;
+   } 
+   public void audioRun(){
+      AudioInputStream stream;
+		AudioFormat format;
+		DataLine.Info info;
+		Clip clip;
+        while(true){
+			stream = AudioSystem.getAudioInputStream(song);
+			format = stream.getFormat();
+			info = new DataLine.Info(Clip.class, format);
+			clip = (Clip)AudioSystem.getLine(info);
+			clip.open(stream);
+			clip.start();			
+		}//end while loop  	
+   }
 }
